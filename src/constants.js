8c9d34a8e28dd3eb101784d3a4e56e236ce5cb71
Inf1n1Ty_75 <volkandu75@gmail.com>
2024-04-23 01:48:35 +0200

    testing an hyperlink in dialogue

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 export const scaleFactor = 2;
 export const offsetX = 16;
 export const offsetY = 16;
+
 export const dialogueData = {
-	desk: `this is my desk`
+    desk: `this is my desk, check out <a href="https://github.com/Vpekdas/so_long">so_long</a>`
 }
