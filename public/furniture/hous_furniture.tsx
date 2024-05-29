024ba9c1a6219fed1be630eaae8b62b41e44a0ec
Inf1n1Ty_75 <volkandu75@gmail.com>
2024-05-30 01:25:31 +0200

    added indicator :)

--- public/furniture/hous_furniture.tsx
+++ public/furniture/hous_furniture.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="hous_furniture" tilewidth="16" tileheight="16" tilecount="234" columns="13">
- <image source="furniture/hous_furniture.png" width="208" height="288"/>
+ <image source="hous_furniture.png" width="208" height="288"/>
 </tileset>
